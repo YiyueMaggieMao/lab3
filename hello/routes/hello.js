5fb98bf6b49626f115e0d8aa996a39bd00899d36
YiyueMaggieMao <myykitty711@gmail.com>
2021-01-22 13:32:27 +0800

    Node.js practice complete

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,11 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+  if(nameToShow === undefined) {
+    nameToShow = "please give me a name so I can greet you"
+  }
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
